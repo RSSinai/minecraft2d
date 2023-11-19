6caa75e0ee83bdd59c297d8fd414e86af7eabec4
Rony Sinai <rony.sinai@gmail.com>
2023-11-19 02:23:08 +0200

    save

--- weekend_16_11/script.js
+++ weekend_16_11/script.js
@@ -8,18 +8,23 @@ function goToStart() {
 /* -------------------------------------------------------------------------- */
 /*                                GENERATE MAP                                */
 /* -------------------------------------------------------------------------- */
-const boardSize = 7; // Adjust the size of the board
+const boardSize = 12; // Adjust the size of the board
 const gameBoard = document.getElementById("game-board");
 
 // Sample map array - replace this with your own logic
 const map = [
-  [1, 1, 3, 1, 1, 1, 1],
-  [1, 1, 3, 1, 1, 1, 1],
-  [1, 1, 3, 1, 1, 1, 1],
-  [1, 1, 3, 1, 1, 1, 1],
-  [8, 8, 3, 3, 3, 3, 8],
-  [8, 8, 8, 8, 8, 8, 8],
-  [1, 1, 1, 1, 1, 1, 1],
+  [3, 3, 3, 3, 3, 3, 3,3,3,3,3,6],
+  [3, 3, 3, 3, 3, 3, 3,3,3,3,6,6],
+  [3, 3, 3, 3, 3, 3, 3,3,3,6,6,6],
+  [3, 3, 3, 3, 3, 3, 3,3,3,3,9,9],
+  [3, 3, 3, 3, 3, 3, 3,3,3,3,9,9],
+  [3, 3, 3, 3, 3, 3, 3,3,3,5,2,2],
+  [3, 3, 3, 3, 3, 3, 3,3,5,2,2,2],
+  [12, 12, 12, 12, 12, 12, 12,12,2,2,2,2],
+  [8, 8, 8, 12, 12, 12, 12,12,8,8,8,8],
+  [11, 8, 8, 8, 8, 8, 8,8,8,8,8,8],
+  [11, 11, 1, 1, 1, 1, 1,1,1,1,1,1],
+  [11, 11, 11, 1, 1, 4, 4,4,4,4,4,4],
 ];
 
 function createBoard() {
@@ -45,62 +50,67 @@ createBoard();
 // check that right tool is selected clicked on
 // click on the right cell with the right tool, if match delete, else nothing
 
-const elementsToolAxe = Array.from(
-    document.querySelectorAll(".card.tool.axe")
-);
-const elementsToolShovel = Array.from(
-  document.querySelectorAll(".card.tool.shovel")
-);
-const elementsToolPickaxe = Array.from(
-  document.querySelectorAll(".card.tool.pickaxe")
-);
+const elementsToolAxe = Array.from(document.querySelectorAll(".card.tool.axe"));
+const elementsToolShovel = Array.from(document.querySelectorAll(".card.tool.shovel"));
+const elementsToolPickaxe = Array.from(document.querySelectorAll(".card.tool.pickaxe"));
 const elementsCell = Array.from(document.querySelectorAll(".cell"));
+const elementInventory = Array.from(document.querySelectorAll(".last-cell"));
 
 let selectedTool = null;
 
 elementsToolAxe.forEach(function (element) {
   element.addEventListener("click", function () {
-    handleToolSelection(1);
+    handleToolSelection(9);
   });
 });
 
 elementsToolShovel.forEach(function (element) {
   element.addEventListener("click", function () {
-    handleToolSelection(3);
+    handleToolSelection(8);
   });
 });
 
 elementsToolPickaxe.forEach(function (element) {
   element.addEventListener("click", function () {
-    handleToolSelection(8);
+    handleToolSelection(4);
   });
 });
 
 function handleToolSelection(imgNumber) {
-  // Reset background images for all tools
-  elementsToolAxe.forEach((tool) => (tool.style.backgroundImage = ""));
-  elementsToolShovel.forEach((tool) => (tool.style.backgroundImage = ""));
-  elementsToolPickaxe.forEach((tool) => (tool.style.backgroundImage = ""));
-
-  // Set the background image for the selected tool
-  const selectedElement = event.target;
-  selectedElement.style.backgroundImage = `url('./cube_images/${imgNumber}.png')`;
-
-  // Update the selected tool
-  selectedTool = imgNumber;
-
-  // Remove event listeners from all cells
-  elementsCell.forEach((element) =>
-    element.removeEventListener("click", handleCellClick)
-  );
-
-  // Add event listener to cells for the selected tool
-  elementsCell.forEach((element) => {
-    element.addEventListener("click", function () {
-      handleCellClick(element);
-    });
-  });
-}
+    // Ask a question before allowing the user to continue
+    const answer = window.prompt("Answer the question to continue: What is the capital of France?");
+    
+    // Check the answer (you can replace this with your own question and answer logic)
+    if (answer && answer.toLowerCase() === "paris") {
+      // Reset background images for all tools
+      elementsToolAxe.forEach((tool) => (tool.style.backgroundImage = ""));
+      elementsToolShovel.forEach((tool) => (tool.style.backgroundImage = ""));
+      elementsToolPickaxe.forEach((tool) => (tool.style.backgroundImage = ""));
+  
+      // Set the background image for the selected tool
+      const selectedElement = event.target;
+      selectedElement.style.backgroundImage = `url('./cube_images/${imgNumber}.png')`;
+  
+      // Update the selected tool
+      selectedTool = imgNumber;
+  
+      // Remove event listeners from all cells
+      elementsCell.forEach((element) =>
+        element.removeEventListener("click", handleCellClick)
+      );
+  
+      // Add event listener to cells for the selected tool
+      elementsCell.forEach((element) => {
+        element.addEventListener("click", function () {
+          handleCellClick(element);
+        });
+      });
+    } else {
+      // If the answer is incorrect, do not proceed
+      alert("Incorrect answer. You cannot continue with the game.");
+    }
+  }
+  
 
 function handleCellClick(element) {
   let computedStyle = window.getComputedStyle(element);
@@ -108,6 +118,12 @@ function handleCellClick(element) {
 
   if (backgroundImage.includes(`cube_images/${selectedTool}.png`)) {
     element.style.backgroundImage = `url('./cube_images/white.PNG')`;
+    
   }
+  
 }
+
+/* -------------------------------------------------------------------------- */
+/*                                  INVENTORY                                 */
+/* -------------------------------------------------------------------------- */
 
